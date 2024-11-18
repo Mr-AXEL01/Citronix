d324b473b75f394e3cfa71c6d666e452709f5d42
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-18 21:49:14 +0100

    inject the FarmService
    inject the farm service in the farm controller.

--- src/main/java/net/axel/citronix/controller/FarmController.java
+++ src/main/java/net/axel/citronix/controller/FarmController.java
@@ -1,12 +1,16 @@
 package net.axel.citronix.controller;
 
+import lombok.RequiredArgsConstructor;
+import net.axel.citronix.service.FarmService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(FarmController.CONTROLLER_PATH)
 
-
+@RequiredArgsConstructor
 public class FarmController {
     public final static String CONTROLLER_PATH = "/api/v1/farms";
+
+    private final FarmService service;
 }
