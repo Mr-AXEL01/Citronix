41076ae5e50b2b75c380257e949223621a56b9b6
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-18 19:08:04 +0100

    add an implementation of the farmService and annotate it with service and transactional annotation to be sure that we have a bean of it for the IOC.

---
+++ src/main/java/net/axel/citronix/service/impl/FarmServiceImpl.java
@@ -0,0 +1,12 @@
+package net.axel.citronix.service.impl;
+
+import jakarta.transaction.Transactional;
+import net.axel.citronix.domain.dtos.FarmRequestDTO;
+import net.axel.citronix.domain.dtos.FarmResponseDTO;
+import net.axel.citronix.service.FarmService;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class FarmServiceImpl implements FarmService {
+}
