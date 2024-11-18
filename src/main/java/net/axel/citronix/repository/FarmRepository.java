8152fa9c0004248efa243fa6761874f090da0c4e
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-18 18:53:40 +0100

    add annotation to repository
    annotate the farmRepository with repository annotation to provide bean of it for the ioc.

--- src/main/java/net/axel/citronix/repository/FarmRepository.java
+++ src/main/java/net/axel/citronix/repository/FarmRepository.java
@@ -2,6 +2,8 @@ package net.axel.citronix.repository;
 
 import net.axel.citronix.domain.entities.Farm;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FarmRepository extends JpaRepository<Farm, Long> {
 }
