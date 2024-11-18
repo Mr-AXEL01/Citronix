4a9a1f5b641a48811c2608052cb827e785a40692
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-18 19:40:02 +0100

    inject the Farm (repository && mapper)
    inject repository and mapper of the farm in it's service implemntation, and use the anotation @requiredArgConstructor to need to create constructor.

--- src/main/java/net/axel/citronix/service/impl/FarmServiceImpl.java
+++ src/main/java/net/axel/citronix/service/impl/FarmServiceImpl.java
@@ -1,12 +1,26 @@
 package net.axel.citronix.service.impl;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import net.axel.citronix.domain.dtos.FarmRequestDTO;
 import net.axel.citronix.domain.dtos.FarmResponseDTO;
+import net.axel.citronix.mapper.FarmMapper;
+import net.axel.citronix.repository.FarmRepository;
 import net.axel.citronix.service.FarmService;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
+
+@RequiredArgsConstructor
 public class FarmServiceImpl implements FarmService {
+
+    private final FarmRepository repository;
+    private final FarmMapper mapper;
+
+
+    @Override
+    public FarmResponseDTO create(FarmRequestDTO dto) {
+        return null;
+    }
 }
