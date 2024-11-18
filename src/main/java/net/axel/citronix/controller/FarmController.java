c694c38a92b1722b4109ae37496bbc729597cf22
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-18 22:06:23 +0100

    add an endpoint
    set an endpoint to create a farm and return the response of the farm with an HttpStatus = 201 as reponse that the farm created.

--- src/main/java/net/axel/citronix/controller/FarmController.java
+++ src/main/java/net/axel/citronix/controller/FarmController.java
@@ -1,7 +1,15 @@
 package net.axel.citronix.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import net.axel.citronix.domain.dtos.FarmRequestDTO;
+import net.axel.citronix.domain.dtos.FarmResponseDTO;
 import net.axel.citronix.service.FarmService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +21,10 @@ public class FarmController {
     public final static String CONTROLLER_PATH = "/api/v1/farms";
 
     private final FarmService service;
+
+    @PostMapping
+    public ResponseEntity<FarmResponseDTO> create(@RequestBody @Valid FarmRequestDTO dto) {
+        FarmResponseDTO farm = service.create(dto);
+        return new ResponseEntity<>(farm, HttpStatus.CREATED);
+    }
 }
