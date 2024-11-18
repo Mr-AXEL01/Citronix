cd40a0d837413f38512b98f28ca2ad165b7ecef3
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-18 19:06:28 +0100

    declare signature method
    set the method signature to create a farm.

---
+++ src/main/java/net/axel/citronix/service/FarmService.java
@@ -0,0 +1,8 @@
+package net.axel.citronix.service;
+
+import net.axel.citronix.domain.dtos.FarmRequestDTO;
+import net.axel.citronix.domain.dtos.FarmResponseDTO;
+
+public interface FarmService {
+    FarmResponseDTO create(FarmRequestDTO dto);
+}
