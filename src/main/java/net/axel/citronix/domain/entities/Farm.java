cf3c0c70908f59b04ae6559c0cfd81b573fe9bcd
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-18 18:47:17 +0100

    craete entity of the farm
    set the ttributes of the entity farm and use the lombok annotations to define the getter && setter && constructor(withArgs , noArgs)

---
+++ src/main/java/net/axel/citronix/domain/entities/Farm.java
@@ -0,0 +1,37 @@
+package net.axel.citronix.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "farms")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Farm implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String location;
+
+    private Double size;
+
+    private LocalDate creationDate;
+
+//    private List<Field> fields = new ArrayList<>();
+}
