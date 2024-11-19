8d4747c925bb075471665d0f7cbacfefde54dd1d
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-19 15:05:04 +0100

    add object of errors as an attributes
    set the object of errors to have the errors that we get.

--- src/main/java/net/axel/citronix/exception/ErrorResponse.java
+++ src/main/java/net/axel/citronix/exception/ErrorResponse.java
@@ -7,6 +7,7 @@ public record ErrorResponse(
         int statusCode,
         LocalDateTime timestamp,
         String message,
-        String description
+        String description,
+        Object errors
 ) {
 }
