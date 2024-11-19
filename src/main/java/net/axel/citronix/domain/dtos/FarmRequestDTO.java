99f64b6da3836a95687ef2fcc680248a0700a81f
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-19 01:24:04 +0100

    CI-6 set the add of a farm with validation
    set the add of a farm with validation for all field state(null or empty) if the name of the farm is already used.

--- src/main/java/net/axel/citronix/domain/dtos/FarmRequestDTO.java
+++ src/main/java/net/axel/citronix/domain/dtos/FarmRequestDTO.java
@@ -2,10 +2,17 @@ package net.axel.citronix.domain.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import net.axel.citronix.domain.entities.Farm;
+import net.axel.citronix.validation.IsUnique;
 
 public record FarmRequestDTO(
+
+        @IsUnique(entityClass = Farm.class, fieldName = "name", message = "This name is already in use")
         @NotBlank String name,
+
         @NotBlank String location,
-        @NotNull Double size
+
+        @NotNull @Positive Double size
 ) {
 }
