6700a0292fcf79445b3a482338d3f5dd93eb36b4
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-19 22:37:52 +0100

    add list of fields to farm response
    set list of Embedded fields in the farm response.

--- src/main/java/net/axel/citronix/domain/dtos/FarmResponseDTO.java
+++ src/main/java/net/axel/citronix/domain/dtos/FarmResponseDTO.java
@@ -3,10 +3,18 @@ package net.axel.citronix.domain.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.List;
+
 public record FarmResponseDTO(
+
         @NotNull Long id,
+
         @NotBlank String name,
+
         @NotBlank String location,
-        @NotNull Double size
+
+        @NotNull Double size,
+
+        List<EmbeddedFieldDTO> fields
 ) {
 }
