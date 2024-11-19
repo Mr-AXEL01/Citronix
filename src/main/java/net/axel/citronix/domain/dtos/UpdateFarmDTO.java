709d0fc895a8bc2d6b3e49c25b1f67aaae546631
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-19 09:43:48 +0100

    add an update DTO
    set an update DTO , so we can change the details of a Farm , and let the ability to send null values in case client want's to change only some fields , and handle empty values (whiteSpaces) with a regex in the pattern annotations.

---
+++ src/main/java/net/axel/citronix/domain/dtos/UpdateFarmDTO.java
@@ -0,0 +1,22 @@
+package net.axel.citronix.domain.dtos;
+
+import jakarta.validation.constraints.*;
+import net.axel.citronix.domain.entities.Farm;
+import net.axel.citronix.validation.IsUnique;
+
+import java.time.LocalDate;
+
+public record UpdateFarmDTO(
+        @IsUnique(entityClass = Farm.class, fieldName = "name", message = "This name is already in use")
+        @Pattern(regexp = "\\S.*", message = "Name cannot be blank or contain only whitespace")
+        String name,
+
+        @Pattern(regexp = "\\S.*", message = "Location cannot be blank or contain only whitespace")
+        String location,
+
+        @Positive Double size,
+
+        @PastOrPresent(message = "Creation date must be in the past or today")
+        LocalDate creationDate
+) {
+}
