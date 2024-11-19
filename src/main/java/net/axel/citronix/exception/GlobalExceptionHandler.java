423276f3820ad7bab8b1e7a9728320126fc2f4a3
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-19 15:35:21 +0100

    add method handles MethodArgumentNotValidException
    It extracts validation errors from the exception, maps the field names to their respective error messages, and returns a custom ErrorResponse.

--- src/main/java/net/axel/citronix/exception/GlobalExceptionHandler.java
+++ src/main/java/net/axel/citronix/exception/GlobalExceptionHandler.java
@@ -2,15 +2,41 @@ package net.axel.citronix.exception;
 
 import net.axel.citronix.exception.domains.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    public static final String VALIDATION_FAILED_MESSAGE = "Validation failed";
+    public static final String ENTITY_NOT_FOUND_MESSAGE = "Resource Not Found";
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationExceptions(final MethodArgumentNotValidException ex, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            final String fieldName = ((FieldError) error).getField();
+            final String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now(),
+                VALIDATION_FAILED_MESSAGE,
+                request.getDescription(false),
+                errors);
+    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
@@ -18,8 +44,10 @@ public class GlobalExceptionHandler {
         return new ErrorResponse(
                 HttpStatus.NOT_FOUND.value(),
                 LocalDateTime.now(),
-                ex.getMessage(),
-                request.getDescription(false));
+                ENTITY_NOT_FOUND_MESSAGE,
+                request.getDescription(false),
+                ex.getMessage()
+        );
     }
 
     @ExceptionHandler(Exception.class)
@@ -28,7 +56,9 @@ public class GlobalExceptionHandler {
         return new ErrorResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 LocalDateTime.now(),
-                ex.getMessage(),
-                request.getDescription(false));
+                ex.getClass().getSimpleName(),
+                request.getDescription(false),
+                ex.getMessage()
+        );
     }
 }
