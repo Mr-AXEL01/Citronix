53d4f932fec4272a53c4448fae9b4d6ab5abdaef
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-20 03:14:30 +0100

    add an endpoint to create a field
    injec the field service and set the endpoint to create a field.

---
+++ src/main/java/net/axel/citronix/controller/FieldController.java
@@ -0,0 +1,30 @@
+package net.axel.citronix.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.axel.citronix.domain.dtos.CreateFieldDTO;
+import net.axel.citronix.domain.dtos.FieldResponseDTO;
+import net.axel.citronix.service.FieldService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(FieldController.CONTROLLER_PATH)
+
+@RequiredArgsConstructor
+public class FieldController {
+
+    public final static String CONTROLLER_PATH = "/api/v1/fields";
+
+    private final FieldService service;
+
+    @PostMapping
+    public ResponseEntity<FieldResponseDTO> create(@RequestBody @Valid CreateFieldDTO dto) {
+        FieldResponseDTO field = service.create(dto);
+        return new ResponseEntity<>(field, HttpStatus.CREATED);
+    }
+}
