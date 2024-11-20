1b4b62b2ca221d35d02342e489dd2a150448912c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-20 14:59:38 +0100

    add entity of the harvest
    set an entity of harvest with it's attributes and implement the relation with harvest detail (1-> *) by setting a list of harvest detail , and the relation with sale  (1 -> *) by setting a list of sales.

---
+++ src/main/java/net/axel/citronix/domain/entities/Harvest.java
@@ -0,0 +1,39 @@
+package net.axel.citronix.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import net.axel.citronix.domain.enums.Season;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Harvests")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Harvest implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Season season;
+
+    private LocalDate harvestDate;
+
+    private Double totalQuantity;
+
+    @OneToMany(mappedBy = "harvest", cascade = CascadeType.ALL)
+    private List<HarvestDetail> harvestDetails = new ArrayList<>();
+
+    @OneToMany(mappedBy = "harvest", cascade = CascadeType.ALL)
+    private List<Sale> sales = new ArrayList<>();
+}
