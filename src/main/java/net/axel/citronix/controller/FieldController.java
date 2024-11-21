3635c3272b46d7532d1ab220d0ee26defddaacbf
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-21 22:18:09 +0100

    add endpoint for the update of the field
    set new endpoint to update a field

--- src/main/java/net/axel/citronix/controller/FieldController.java
+++ src/main/java/net/axel/citronix/controller/FieldController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import net.axel.citronix.domain.dtos.field.CreateFieldDTO;
 import net.axel.citronix.domain.dtos.field.FieldResponseDTO;
+import net.axel.citronix.domain.dtos.field.UpdateFieldDTO;
 import net.axel.citronix.service.FieldService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,5 +30,12 @@ public class FieldController {
     public ResponseEntity<FieldResponseDTO> findById(@PathVariable("id") Long id) {
         FieldResponseDTO farm = service.findById(id);
         return ResponseEntity.ok(farm);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FieldResponseDTO> update(@PathVariable("id") Long id,
+                                                   @RequestBody @Valid UpdateFieldDTO dto) {
+        FieldResponseDTO updatedField = service.update(id, dto);
+        return ResponseEntity.ok(updatedField);
     }
 }
