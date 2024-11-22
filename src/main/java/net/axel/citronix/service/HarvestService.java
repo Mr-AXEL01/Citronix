4a60fd886ed82bcd7a6299db7115414e13af24c7
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-22 19:22:31 +0100

    declare signature methods for the harvest interface.

--- src/main/java/net/axel/citronix/service/HarvestService.java
+++ src/main/java/net/axel/citronix/service/HarvestService.java
@@ -3,7 +3,17 @@ package net.axel.citronix.service;
 import net.axel.citronix.domain.dtos.harvest.CreateHarvestDTO;
 import net.axel.citronix.domain.dtos.harvest.HarvestResponseDTO;
 
+import java.util.List;
+
 public interface HarvestService {
 
+    HarvestResponseDTO findById(Long id);
+
+    List<HarvestResponseDTO> findAll();
+
     HarvestResponseDTO create(CreateHarvestDTO dto);
+
+    HarvestResponseDTO update(Long id);
+
+    void delete(Long id);
 }
