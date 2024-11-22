ed8233fec9f6a06875f4188acfb2a129ce9bb3ff
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-22 19:21:27 +0100

    add an update  dto
    set an update dto for the harvest.

---
+++ src/main/java/net/axel/citronix/domain/dtos/harvest/UpdateHarvestDTO.java
@@ -0,0 +1,9 @@
+package net.axel.citronix.domain.dtos.harvest;
+
+import java.time.LocalDate;
+
+public record UpdateHarvestDTO(
+        LocalDate harvestDate,
+        Double totalQuantity
+) {
+}
