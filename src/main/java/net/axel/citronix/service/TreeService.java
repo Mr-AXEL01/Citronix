d56a4c4bf114201ee69099aba0661e4ea0fc163b
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-22 03:14:12 +0100

    declare new method
    set the method signature for the create method.

--- src/main/java/net/axel/citronix/service/TreeService.java
+++ src/main/java/net/axel/citronix/service/TreeService.java
@@ -1,4 +1,9 @@
 package net.axel.citronix.service;
 
+import net.axel.citronix.domain.dtos.tree.CreateTreeDTO;
+import net.axel.citronix.domain.dtos.tree.TreeResponseDTO;
+
 public interface TreeService {
+
+    TreeResponseDTO create(CreateTreeDTO dto);
 }
