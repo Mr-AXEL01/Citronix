8efa737b179b028e59a3cd5caccbd811f27d98d6
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-22 05:00:25 +0100

    add an endpoint
    set an endpoint to create a tree.

--- src/main/java/net/axel/citronix/controller/TreeController.java
+++ src/main/java/net/axel/citronix/controller/TreeController.java
@@ -1,7 +1,14 @@
 package net.axel.citronix.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import net.axel.citronix.domain.dtos.tree.CreateTreeDTO;
+import net.axel.citronix.domain.dtos.tree.TreeResponseDTO;
 import net.axel.citronix.service.TreeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +21,10 @@ public class TreeController {
     public final static String CONTROLLER_PATH = "/api/v1/trees";
 
     private final TreeService service;
+
+    @PostMapping
+    public ResponseEntity<TreeResponseDTO> create(@RequestBody @Valid CreateTreeDTO dto) {
+        TreeResponseDTO tree = service.create(dto);
+        return new ResponseEntity<>(tree, HttpStatus.CREATED);
+    }
 }
