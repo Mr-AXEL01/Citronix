6fe70ac89da6be28f3fff7068d841ad364ef5e69
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-22 16:26:26 +0100

    add an endpoint to harvest
    set an endpoint to create new harvest.

--- src/main/java/net/axel/citronix/controller/HarvestController.java
+++ src/main/java/net/axel/citronix/controller/HarvestController.java
@@ -1,7 +1,14 @@
 package net.axel.citronix.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import net.axel.citronix.domain.dtos.harvest.CreateHarvestDTO;
+import net.axel.citronix.domain.dtos.harvest.HarvestResponseDTO;
 import net.axel.citronix.service.HarvestService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class HarvestController {
 
-    public final static String CONTROLLER_PATH = "/api/v1/harvest";
+    public final static String CONTROLLER_PATH = "/api/v1/harvests";
 
     private final HarvestService service;
+    
+    @PostMapping
+    public ResponseEntity<HarvestResponseDTO> create(@RequestBody @Valid CreateHarvestDTO dto) {
+        HarvestResponseDTO harvest = service.create(dto);
+        return new ResponseEntity<>(harvest, HttpStatus.CREATED);
+    }
 }
