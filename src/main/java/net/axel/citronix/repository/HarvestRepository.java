b3f06bd1eedafc8b163910df61c182c16e56881b
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-22 15:42:56 +0100

    add boolean method to check if there is a harvest in the season.

--- src/main/java/net/axel/citronix/repository/HarvestRepository.java
+++ src/main/java/net/axel/citronix/repository/HarvestRepository.java
@@ -1,9 +1,11 @@
 package net.axel.citronix.repository;
 
 import net.axel.citronix.domain.entities.Harvest;
+import net.axel.citronix.domain.enums.Season;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HarvestRepository extends JpaRepository<Harvest, Long> {
+    Boolean existsBySeason(Season season);
 }
