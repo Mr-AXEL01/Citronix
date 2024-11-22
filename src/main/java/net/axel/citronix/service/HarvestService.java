5082afdd24c0497d13319b63e7e767c1565c8347
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-22 19:45:41 +0100

    updte method signature
    change the method signature by adding a param (updateHarvestDTO).

--- src/main/java/net/axel/citronix/service/HarvestService.java
+++ src/main/java/net/axel/citronix/service/HarvestService.java
@@ -2,6 +2,7 @@ package net.axel.citronix.service;
 
 import net.axel.citronix.domain.dtos.harvest.CreateHarvestDTO;
 import net.axel.citronix.domain.dtos.harvest.HarvestResponseDTO;
+import net.axel.citronix.domain.dtos.harvest.UpdateHarvestDTO;
 
 import java.util.List;
 
@@ -13,7 +14,7 @@ public interface HarvestService {
 
     HarvestResponseDTO create(CreateHarvestDTO dto);
 
-    HarvestResponseDTO update(Long id);
+    HarvestResponseDTO update(Long id, UpdateHarvestDTO dto);
 
     void delete(Long id);
 }
