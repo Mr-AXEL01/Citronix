cd8e690dd49786679642e902bf97a258ed580f34
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-22 03:15:33 +0100

    inject beans
    inject the Tree (repository && mapper) and Field (service && mapper)

--- src/main/java/net/axel/citronix/service/impl/TreeServiceImpl.java
+++ src/main/java/net/axel/citronix/service/impl/TreeServiceImpl.java
@@ -1,10 +1,24 @@
 package net.axel.citronix.service.impl;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import net.axel.citronix.mapper.FieldMapper;
+import net.axel.citronix.mapper.TreeMapper;
+import net.axel.citronix.repository.TreeRepository;
+import net.axel.citronix.service.FieldService;
 import net.axel.citronix.service.TreeService;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
+
+@RequiredArgsConstructor
 public class TreeServiceImpl implements TreeService {
+
+    private final TreeRepository repository;
+    private final TreeMapper mapper;
+    private final FieldService fieldService;
+    private final FieldMapper fieldMapper;
+
+
 }
