f24254ebb9fb085fa900bb578f49850e65c73e10
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-22 03:12:39 +0100

    remove age
    remove the age from the create tree dto , and set it depends on the plantingDate, that is validate with the annotation @pastOrPresent.

--- src/main/java/net/axel/citronix/domain/dtos/tree/CreateTreeDTO.java
+++ src/main/java/net/axel/citronix/domain/dtos/tree/CreateTreeDTO.java
@@ -1,12 +1,18 @@
 package net.axel.citronix.domain.dtos.tree;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import net.axel.citronix.domain.entities.Field;
+import net.axel.citronix.validation.Exists;
 
 import java.time.LocalDate;
 
 public record CreateTreeDTO(
-        LocalDate plantingDate,
-        @NotNull Integer age,
+
+        @PastOrPresent(message = "Planting date must be in the past or today.")
+        @NotNull LocalDate plantingDate,
+
+        @Exists(entityClass = Field.class, fieldName = "id", message = "Field does not exists.")
         @NotNull Long fieldId
 ) {
 }
