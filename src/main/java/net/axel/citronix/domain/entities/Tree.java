3c1583326348272c62552125095493475de94a75
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-23 22:09:49 +0100

    add transiet annotation for age && productivity
    set transiet annotation to be sure that the age and productivity not stored , cause they are calculable

--- src/main/java/net/axel/citronix/domain/entities/Tree.java
+++ src/main/java/net/axel/citronix/domain/entities/Tree.java
@@ -1,6 +1,7 @@
 package net.axel.citronix.domain.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,8 +28,10 @@ public class Tree implements Serializable {
 
     private LocalDate plantingDate;
 
+    @Transient
     private Integer age;
 
+    @Transient
     private Double productivity;
 
     @ManyToOne
@@ -36,4 +40,30 @@ public class Tree implements Serializable {
 
     @OneToMany(mappedBy = "tree", cascade = CascadeType.ALL)
     private List<HarvestDetail> harvestDetails = new ArrayList<>();
+
+    public Double getProductivity(int age) {
+        return getaDouble(age);
+    }
+
+    public Integer getAge(LocalDate plantingDate) {
+        if (plantingDate == null) {
+            throw new IllegalStateException("Planting date is required to calculate age.");
+        }
+        return getaInteger(plantingDate);
+    }
+
+    @NotNull
+    public static Double getaDouble(int age) {
+        return switch (age) {
+            case 1, 2 -> 2.5;
+            case 3, 4, 5, 6, 7, 8, 9, 10 -> 12.0;
+            case 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 -> 20.0;
+            default -> 0.0;
+        };
+    }
+
+    @NotNull
+    public static Integer getaInteger(LocalDate plantingDate) {
+        return (int) ChronoUnit.YEARS.between(plantingDate, LocalDate.now());
+    }
 }
