b344cd9b458b9ccfd5ef5057339a033b0f3e2538
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-23 21:15:01 +0100

    add custome constructor

--- src/main/java/net/axel/citronix/domain/entities/HarvestDetail.java
+++ src/main/java/net/axel/citronix/domain/entities/HarvestDetail.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import net.axel.citronix.domain.embeddeds.HarvestDetailKey;
+import org.hibernate.annotations.processing.HQL;
 
 import java.io.Serializable;
 
@@ -26,10 +27,17 @@ public class HarvestDetail implements Serializable {
     @JoinColumn(name = "tree_id", nullable = false)
     private Tree tree;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @MapsId("harvestId")
     @JoinColumn(name = "harvest_id", nullable = false)
     private Harvest harvest;
 
     private Double quantity;
+
+    public HarvestDetail(Tree tree, Harvest harvest, Double quantity) {
+        this.id = new HarvestDetailKey(tree.getId(), harvest.getId());
+        this.tree = tree;
+        this.harvest = harvest;
+        this.quantity = quantity;
+    }
 }
