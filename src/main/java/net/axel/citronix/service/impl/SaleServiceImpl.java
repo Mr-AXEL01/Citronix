87c2a74f7ee06cc0ff176e1a54eed571e32df785
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-24 06:56:39 +0100

    add new method implementation
    implement the findById() where it get the sale that we want and if it's not exists it throw an exception of resource not found , and if it\s exist we map it to return the response of sale.

--- src/main/java/net/axel/citronix/service/impl/SaleServiceImpl.java
+++ src/main/java/net/axel/citronix/service/impl/SaleServiceImpl.java
@@ -1,10 +1,27 @@
 package net.axel.citronix.service.impl;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import net.axel.citronix.domain.dtos.sale.SaleResponseDTO;
+import net.axel.citronix.exception.domains.ResourceNotFoundException;
+import net.axel.citronix.mapper.SaleMapper;
+import net.axel.citronix.repository.SaleRepository;
 import net.axel.citronix.service.SaleService;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
+
+@RequiredArgsConstructor
 public class SaleServiceImpl implements SaleService {
+
+    private final SaleRepository repository;
+    private final SaleMapper mapper;
+
+    @Override
+    public SaleResponseDTO findById(Long id) {
+        return repository.findById(id)
+                .map(mapper::toResponseDto)
+                .orElseThrow(() -> new ResourceNotFoundException("sale", id));
+    }
 }
