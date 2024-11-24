6300e003ad213c00c0e49a8298a53f8328f84f3c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-24 07:49:52 +0100

    add ne method to create a sale
    implement new method to create a sale nd before that check if there an exist one by harvest .

--- src/main/java/net/axel/citronix/service/impl/SaleServiceImpl.java
+++ src/main/java/net/axel/citronix/service/impl/SaleServiceImpl.java
@@ -2,11 +2,17 @@ package net.axel.citronix.service.impl;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import net.axel.citronix.domain.dtos.harvest.HarvestResponseDTO;
+import net.axel.citronix.domain.dtos.sale.CreateSaleDTO;
 import net.axel.citronix.domain.dtos.sale.SaleResponseDTO;
+import net.axel.citronix.domain.entities.Harvest;
 import net.axel.citronix.domain.entities.Sale;
+import net.axel.citronix.exception.domains.BusinessException;
 import net.axel.citronix.exception.domains.ResourceNotFoundException;
+import net.axel.citronix.mapper.HarvestMapper;
 import net.axel.citronix.mapper.SaleMapper;
 import net.axel.citronix.repository.SaleRepository;
+import net.axel.citronix.service.HarvestService;
 import net.axel.citronix.service.SaleService;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +26,8 @@ public class SaleServiceImpl implements SaleService {
 
     private final SaleRepository repository;
     private final SaleMapper mapper;
+    private final HarvestService harvestService;
+    private final HarvestMapper harvestMapper;
 
     @Override
     public SaleResponseDTO findById(Long id) {
@@ -39,5 +47,22 @@ public class SaleServiceImpl implements SaleService {
         return saleEntities.stream()
                 .map(mapper::toResponseDto)
                 .toList();
+    }
+
+    @Override
+    public SaleResponseDTO create(CreateSaleDTO dto) {
+        HarvestResponseDTO harvestResponse = harvestService.findById(dto.harvestId());
+        Harvest harvest = harvestMapper.toEntityFromResponseDto(harvestResponse);
+
+        if (repository.existsByHarvest(harvest)) {
+            throw new BusinessException("This harvest is already sold.");
+        }
+
+        Sale sale = mapper.toEntity(dto)
+                .setHarvest(harvest);
+
+        Sale savedSale = repository.save(sale);
+
+        return mapper.toResponseDto(savedSale);
     }
 }
