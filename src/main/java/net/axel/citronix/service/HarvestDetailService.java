11beaf0e8218c56bbfbcb78b080defed002f65f9
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-24 06:04:10 +0100

    CI-15 handle the harvest details
    handle the harvest details where im throwing an exception in case the detail that we want to update not exists , and if the new quantity not null and defferent than the exist one set the new one and we change on the total quantity in the harvest related .

--- src/main/java/net/axel/citronix/service/HarvestDetailService.java
+++ src/main/java/net/axel/citronix/service/HarvestDetailService.java
@@ -1,4 +1,9 @@
 package net.axel.citronix.service;
 
+import net.axel.citronix.domain.dtos.harvestDetail.CreateHarvestDetailDTO;
+import net.axel.citronix.domain.dtos.harvestDetail.HarvestDetailResponseDTO;
+
 public interface HarvestDetailService {
+
+    HarvestDetailResponseDTO update(CreateHarvestDetailDTO dto);
 }
--- src/main/java/net/axel/citronix/service/impl/HarvestDetailServiceImpl.java
+++ src/main/java/net/axel/citronix/service/impl/HarvestDetailServiceImpl.java
@@ -1,10 +1,44 @@
 package net.axel.citronix.service.impl;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import net.axel.citronix.domain.dtos.harvestDetail.CreateHarvestDetailDTO;
+import net.axel.citronix.domain.dtos.harvestDetail.HarvestDetailResponseDTO;
+import net.axel.citronix.domain.embeddeds.HarvestDetailKey;
+import net.axel.citronix.domain.entities.Harvest;
+import net.axel.citronix.domain.entities.HarvestDetail;
+import net.axel.citronix.exception.domains.ResourceNotFoundException;
+import net.axel.citronix.mapper.HarvestDetailMapper;
+import net.axel.citronix.repository.HarvestDetailRepository;
 import net.axel.citronix.service.HarvestDetailService;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
+
+@RequiredArgsConstructor
 public class HarvestDetailServiceImpl implements HarvestDetailService {
+
+    private final HarvestDetailRepository repository;
+    private final HarvestDetailMapper mapper;
+
+    @Override
+    public HarvestDetailResponseDTO update(CreateHarvestDetailDTO dto) {
+        HarvestDetailKey id = new HarvestDetailKey(dto.treeId(), dto.harvestId());
+        HarvestDetail existingHarvestDetail = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("HarvestDetail", id));
+
+        if (dto.quantity() != null && !dto.quantity().equals(existingHarvestDetail.getQuantity())) {
+            double newQuantity = dto.quantity();
+            double oldQuantity = existingHarvestDetail.getQuantity();
+            double difference = newQuantity - oldQuantity;
+
+            existingHarvestDetail.setQuantity(newQuantity);
+
+            Harvest harvest = existingHarvestDetail.getHarvest();
+            harvest.setTotalQuantity(harvest.getTotalQuantity() + difference);
+        }
+
+        return mapper.toResponseDto(existingHarvestDetail);
+    }
 }
