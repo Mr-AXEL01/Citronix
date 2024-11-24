bc8b14d196911d26cf74c13cc559c7f3fa59f4f3
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-24 06:51:30 +0100

    add methods signature
    set the methods signature for the crud operations.

--- src/main/java/net/axel/citronix/service/SaleService.java
+++ src/main/java/net/axel/citronix/service/SaleService.java
@@ -1,4 +1,19 @@
 package net.axel.citronix.service;
 
+import net.axel.citronix.domain.dtos.sale.CreateSaleDTO;
+import net.axel.citronix.domain.dtos.sale.SaleResponseDTO;
+
+import java.util.List;
+
 public interface SaleService {
+
+    SaleResponseDTO create(CreateSaleDTO dto);
+
+    SaleResponseDTO update();
+
+    SaleResponseDTO findById(Long id);
+
+    List<SaleResponseDTO> findAll();
+
+    void delete(Long id);
 }
