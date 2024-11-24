4b4ff866e576def11a034f4cb8cdb81848207402
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-24 05:59:18 +0100

    add new enpoint for the harvest details
    set an endpoint to change the quantity of a tree.

--- src/main/java/net/axel/citronix/controller/HarvestDetailController.java
+++ src/main/java/net/axel/citronix/controller/HarvestDetailController.java
@@ -1,7 +1,13 @@
 package net.axel.citronix.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import net.axel.citronix.domain.dtos.harvestDetail.CreateHarvestDetailDTO;
+import net.axel.citronix.domain.dtos.harvestDetail.HarvestDetailResponseDTO;
 import net.axel.citronix.service.HarvestDetailService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +20,10 @@ public class HarvestDetailController {
     public final static String CONTROLLER_PATH = "/api/v1/harvest-details";
 
     private final HarvestDetailService service;
+
+    @PutMapping
+    public ResponseEntity<HarvestDetailResponseDTO> update(@RequestBody @Valid CreateHarvestDetailDTO dto) {
+        HarvestDetailResponseDTO updatedHarvestDetail = service.update(dto);
+        return ResponseEntity.ok(updatedHarvestDetail);
+    }
 }
