8e7609e9d2d5f6fb8fb6f0736c21f7e3be4409d3
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-24 07:14:18 +0100

    add new method
    implement the find all method and set an exception to be throwen in case no data was found.

--- src/main/java/net/axel/citronix/service/impl/SaleServiceImpl.java
+++ src/main/java/net/axel/citronix/service/impl/SaleServiceImpl.java
@@ -3,11 +3,14 @@ package net.axel.citronix.service.impl;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import net.axel.citronix.domain.dtos.sale.SaleResponseDTO;
+import net.axel.citronix.domain.entities.Sale;
 import net.axel.citronix.exception.domains.ResourceNotFoundException;
 import net.axel.citronix.mapper.SaleMapper;
 import net.axel.citronix.repository.SaleRepository;
 import net.axel.citronix.service.SaleService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -23,5 +26,18 @@ public class SaleServiceImpl implements SaleService {
         return repository.findById(id)
                 .map(mapper::toResponseDto)
                 .orElseThrow(() -> new ResourceNotFoundException("sale", id));
+    }
+
+    @Override
+    public List<SaleResponseDTO> findAll() {
+        List<Sale> saleEntities = repository.findAll();
+
+        if(saleEntities.isEmpty()) {
+            throw new ResourceNotFoundException("No sales was found.");
+        }
+
+        return saleEntities.stream()
+                .map(mapper::toResponseDto)
+                .toList();
     }
 }
