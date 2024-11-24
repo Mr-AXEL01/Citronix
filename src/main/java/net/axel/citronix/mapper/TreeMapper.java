1c687c133e76061e3ccfca16d5ef58219c4a6175
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-24 21:57:15 +0100

    add mapping annotation
    set the mapping annotation to specify the target of the age and productivity and map them.

--- src/main/java/net/axel/citronix/mapper/TreeMapper.java
+++ src/main/java/net/axel/citronix/mapper/TreeMapper.java
@@ -4,7 +4,12 @@ import net.axel.citronix.domain.dtos.tree.CreateTreeDTO;
 import net.axel.citronix.domain.dtos.tree.TreeResponseDTO;
 import net.axel.citronix.domain.entities.Tree;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TreeMapper extends BaseMapper<Tree, CreateTreeDTO, TreeResponseDTO> {
+
+    @Mapping(target = "age", expression = "java(tree.getAge(tree.getPlantingDate()))")
+    @Mapping(target = "productivity", expression = "java(tree.getProductivity(tree.getAge(tree.getPlantingDate())))")
+    TreeResponseDTO toResponseDto(Tree tree);
 }
