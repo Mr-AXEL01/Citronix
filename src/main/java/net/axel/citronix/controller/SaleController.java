b0aa32cd69cb7473c2bcdb8ccd8e9bef8a6664b9
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-24 18:21:20 +0100

    CI-16 set the endpoints for the sale
    set all crud endpoints for the sale.

--- src/main/java/net/axel/citronix/controller/SaleController.java
+++ src/main/java/net/axel/citronix/controller/SaleController.java
@@ -1,9 +1,16 @@
 package net.axel.citronix.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import net.axel.citronix.domain.dtos.sale.CreateSaleDTO;
+import net.axel.citronix.domain.dtos.sale.SaleResponseDTO;
+import net.axel.citronix.domain.dtos.sale.UpdateSaleDTO;
 import net.axel.citronix.service.SaleService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(SaleController.CONTROLLER_PATH)
@@ -14,4 +21,36 @@ public class SaleController {
     public final static String CONTROLLER_PATH = "/api/v1/sales";
 
     private final SaleService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SaleResponseDTO> findById(@PathVariable("id") Long id) {
+        SaleResponseDTO sale = service.findById(id);
+        return new ResponseEntity<>(sale, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SaleResponseDTO>> findAll() {
+        List<SaleResponseDTO> sales = service.findAll();
+        return ResponseEntity.ok(sales);
+    }
+
+    @PostMapping
+    public ResponseEntity<SaleResponseDTO> create(@RequestBody @Valid CreateSaleDTO dto) {
+        SaleResponseDTO sale = service.create(dto);
+        return new ResponseEntity<>(sale, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SaleResponseDTO> update(@PathVariable("id") Long id,
+                                                  @RequestBody @Valid UpdateSaleDTO dto) {
+        SaleResponseDTO updatedSale = service.update(id, dto);
+        return ResponseEntity.ok(updatedSale);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
