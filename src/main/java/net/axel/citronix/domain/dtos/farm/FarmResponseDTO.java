0aeeec795f4f3298a68857d9e1ccca2b8128b9a2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-11-25 03:39:50 +0100

    set the creation date to be returned with response dto.

--- src/main/java/net/axel/citronix/domain/dtos/farm/FarmResponseDTO.java
+++ src/main/java/net/axel/citronix/domain/dtos/farm/FarmResponseDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import net.axel.citronix.domain.dtos.field.EmbeddedFieldDTO;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public record FarmResponseDTO(
@@ -15,6 +16,8 @@ public record FarmResponseDTO(
         @NotBlank String location,
 
         @NotNull Double size,
+
+        @NotNull LocalDate creationDate,
 
         List<EmbeddedFieldDTO> fields
 ) {
